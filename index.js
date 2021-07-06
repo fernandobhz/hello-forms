3b715e31ccc034fae28482e51d10c97d03f40ddb
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-07-06 15:11:26 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/usuarios/editar/:id', (req, res) => {
+  res.send(`
+    Dados recebidos para o usuário: ${req.params.id}
+
+    ${JSON.stringify(req.body, null, 2)}
+  `);
+})
+
+app.listen(PORT, () => console.log(`Server is up and running on port: ${PORT}`));
